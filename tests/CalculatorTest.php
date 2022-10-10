02e3e6e6bc02cc894c30bbef12a0a15e5ffdeb48
Anti Ainsar <antiainsar@gmail.com>
2022-10-10 21:56:06 +0300

    commit 6, refactoring the test code

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -3,42 +3,50 @@
 class CalculatorTest extends \PHPUnit\Framework\TestCase {
     public function testAdd () {
 
-        $result = 20 + 5;
+        $calculator = new App\Calculator;
+
+        $result = $calculator->add(20,5);
         $this->assertEquals(25, $result);
 
     }
     
     public function testSubtract () {
 
-        $result = 20 - 5;
+        $calculator = new App\Calculator;
+        $result = $calculator->subtract(20,5);
+
         $this->assertEquals(15, $result);
 
     }
 
     public function testMultiply () {
 
-        $result = 20 * 5;
+        $calculator = new App\Calculator;
+        $result = $calculator->multiply(20,5);
         $this->assertEquals(100, $result);
 
     }
 
     public function testDivide () {
 
-        $result = 20 / 5;
+        $calculator = new App\Calculator;
+        $result = $calculator->divide(20,5);
         $this->assertEquals(4, $result);
 
     }
 
     public function testSquare () {
 
-        $result = pow(4,2);
+        $calculator = new App\Calculator;
+        $result = $calculator->square(4,2);
         $this->assertEquals(16, $result);
 
     }
     
     public function testSquareRoot () {
 
-        $result = sqrt(16);
+        $calculator = new App\Calculator;
+        $result = $calculator->squareroot(16,0);
         $this->assertEquals(4, $result);
 
     }
